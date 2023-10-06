ae64c1e8be93feea6099826be3f7fe24efe02c79
unknown <fj_rh@hotmail.com>
2023-10-06 18:23:21 -0400

    portfolio added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import NameContainer from "./modules/title/socket";
 import Body from "./modules/body/body";
 import TechList from "./modules/tech/tech";
+import Portfolio from "./modules/portafolio/portafolio";
 
 
 function App() {
@@ -11,7 +12,9 @@ function App() {
       <div id="rectangle-background"></div>
       <div id="rectangle-background2"></div>
       <NameContainer/>
+    
       <TechList/>
+      <Portfolio/>
       <Body/>
     </div>
   );
